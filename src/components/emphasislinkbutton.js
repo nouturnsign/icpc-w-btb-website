57e59188e0fcc82d88c99e0140df7b1d18cb39fd
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-10-14 22:22:03 -0700

    Fix emphasis link button

--- src/components/emphasislinkbutton.js
+++ src/components/emphasislinkbutton.js
@@ -1,17 +1,20 @@
-import { Button, Link } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 const EmphasisLinkButton = ({ url, text }) => {
   return (
-    <Link to={url} target="_blank" rel="noopener noreferrer">
-      <Button
-        m={3}
-        fontSize={["1xl", "2xl"]}
-        colorScheme="brand"
-        variant="outline"
-      >
-        {text}
-      </Button>
-    </Link>
+    <Button
+      m={3}
+      fontSize={["1xl", "2xl"]}
+      colorScheme="brand"
+      variant="outline"
+      as="a"
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      size="md"
+    >
+      {text}
+    </Button>
   );
 };
 
