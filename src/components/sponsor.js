078ff61aa0d125f25de09cae63fecd83d502b974
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-10-18 13:36:38 -0700

    Make sponsor image width fixed

--- src/components/sponsor.js
+++ src/components/sponsor.js
@@ -1,8 +1,6 @@
-import { Image, Link, useMediaQuery } from "@chakra-ui/react";
+import { Image, Link } from "@chakra-ui/react";
 
 const Sponsor = ({ sponsor_name, external_link, img_src }) => {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-
   return (
     <Link href={external_link} isExternal>
       <Image
@@ -11,7 +9,7 @@ const Sponsor = ({ sponsor_name, external_link, img_src }) => {
         my="10px"
         objectFit="fit"
         src={img_src}
-        width={isMobile ? "80%" : "200px"}
+        width="200px"
       />
     </Link>
   );
